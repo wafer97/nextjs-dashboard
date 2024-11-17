1d1ff5eb30f131e73dd9028256da52510d4894c7
Ryan Chen <bob798875941@qq.com>
2024-11-18 01:47:30 +0800

    The first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+// add a secondary font
+export const lusita = Lusitana({ weight: ['400'], subsets: ['latin'] });
