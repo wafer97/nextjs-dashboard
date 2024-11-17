4f40b7807f154014a9ccd97be1e4a39aad7f3fdd
Ryan Chen <bob798875941@qq.com>
2024-11-18 02:40:28 +0800

    change lusita to lusitana for avoiding errors

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 // add a secondary font
-export const lusita = Lusitana({ weight: ['400'], subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400'], subsets: ['latin'] });
